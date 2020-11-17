9bfb9f3c2d6bb7a35ee481706d34a899a1cc7a5b
SamyKhelfa <thebboysams@gmail.com>
2020-11-17 14:33:46 +0100

    update

---
+++ server.js
@@ -0,0 +1,23 @@
+const net = require('net')
+
+const server = net.createServer((socket) => {
+  console.log('new connection')
+
+  socket.on('data', (data) => {
+    const [directive, parameter] = data.toString().split(' ')
+
+    switch(directive) {
+        case 'USER':
+            // check if user exist in database
+            // if true
+            socket.write('200 successfuly connected')
+            break;
+    }
+  })
+
+  socket.write('Hello from server')
+})
+
+server.listen(5000, () => {
+  console.log('Server started at port 5000')
+})  
