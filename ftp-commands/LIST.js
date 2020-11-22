602e73806afa3f0124c9a7c90a4d2b055399319c
SamyKhelfa <thebboysams@gmail.com>
2020-11-22 20:56:14 +0100

    update

--- ftp-commands/LIST.js
+++ ftp-commands/LIST.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.readdir(path, function(err, files) {
-    console.log(files);
+module.exports = function list(){
 
-    for (var i=0; i<files.length; i++) {
-        console.log(files[i]);
-    }
-});
-
-console.log
+    fs.readdir(path, function(err, files) {
+        console.log(files);
+    
+        for (var i=0; i<files.length; i++) {
+            console.log(files[i]);
+        }
+    });
+}
